464d4738e977d7fc1293739329bccb388ed347a3
tulipAI <wangsaisai00@gmail.com>
2019-02-11 20:58:51 -0800

    Final

--- twitterCrawler/main.py
+++ twitterCrawler/main.py
@@ -4,14 +4,7 @@ from tweepy.streaming import StreamListener
 import MySQLdb
 import time
 import json
-
-#                        server       MySQL username	MySQL pass  Database name.
-#conn = MySQLdb.connect("mysql.server", "beginneraccount", "cookies", "beginneraccount$tutorial")
-
-#c = conn.cursor()
-
-# consumer key, consumer secret, access token, access secret.
-
+import sys
 
 ckey = "YyUi54vOJ5GsfQbVvxAeSiL35"
 csecret = "ODSZaNg4OGqXYYjSBaMiwqDKhKrHsDaLrctaxqm2n754PpAndM"
@@ -19,7 +12,12 @@ atoken = "527831126-2j4HHl57mrhVErE4o9EqKAG4EmYzYPIDXQjvPArg"
 asecret = "BulKS8mipUEifCTEw96MzsoI429S7i49SDUK2AIi3hydc"
 
 
+count = int(sys.argv[1])
+out_file = sys.argv[2]
+print sys.argv
+
 class listener(StreamListener):
+    i = 0
     def on_data(self, data):
         all_data = json.loads(data)
         dictfilt = lambda x, y: dict([(i, x[i]) for i in x if i in set(y)])
@@ -30,9 +28,18 @@ class listener(StreamListener):
 
             want_keys = ('text', 'timestamp_ms', 'coordinates', 'user', 'entities')
             wanted_data = dictfilt(all_data, want_keys)
-            # print str(wanted_data)
-            with open('data.json', 'a') as tf:
-                json.dump(wanted_data, tf)
+
+            if self.i == 0:
+                with open(out_file, 'a') as f:
+                    f.write("["+json.dumps(wanted_data, indent=2))
+            elif self.i < count:
+                with open(out_file, 'a') as f:
+                    f.write("," + json.dumps(wanted_data, indent=2))
+            else:
+                with open(out_file, 'a') as f:
+                    f.write("]")
+                return False
+            self.i += 1
         return True
 
     def on_error(self, status):
@@ -46,3 +53,5 @@ auth.set_access_token(atoken, asecret)
 
 twitterStream = Stream(auth, listener())
 twitterStream.filter(locations = GEOBOX_WORLD)
+
+
