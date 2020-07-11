79c33426911e2a5fdfd1dfffc6cbd96c697f52ff
tulipAl <44119669+tulipAl@users.noreply.github.com>
2020-07-10 18:48:40 -0700

    Add files via upload

---
+++ build/precache-manifest.97d8bc1dc85f4ba0facc7192c7ea934f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e121df6859779ba6103635b34c22978b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3cfd979ca8fba946b05",
+    "url": "/static/css/main.96df6f37.chunk.css"
+  },
+  {
+    "revision": "713ce41f8fdfd9a3bf2f",
+    "url": "/static/js/2.d54f1e2f.chunk.js"
+  },
+  {
+    "revision": "1d19b0f6582b371c7d45a1a1c7f95d6d",
+    "url": "/static/js/2.d54f1e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cfd979ca8fba946b05",
+    "url": "/static/js/main.69155a35.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/static/js/main.69155a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3fdab3f86a3b0f2aa6",
+    "url": "/static/js/runtime-main.f693082c.js"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
